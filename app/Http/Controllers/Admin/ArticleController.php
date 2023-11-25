e0e16f244d32cdd29517c57d4309cd986686ece8
Waldemar Wilke <wwilke@wp.pl>
2023-11-25 07:58:05 +0100

    pierwsza zmiana

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -10,6 +10,7 @@ class ArticleController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('can:isAdministrator');
     }
 
     /**
