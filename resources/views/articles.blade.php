32c7923c3c8e6fab87925070af4accde6dd85467
waldiw <wwilke@wp.pl>
2023-12-07 14:20:53 +0100

    lista artykułów

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -5,10 +5,21 @@
 @section('content')
     <h2>Artykuły:</h2>
     @if($articles->count() > 0)
+    <div class="containerWrap">
             @foreach($articles as $article)
-                <img src="{{ $article->photo }}" alt="">
+                {{--  <img src="{{ $article->photo }}" alt="">  --}}
+                <div class="wrapW">
+                    <div class="nazwaTowaru">{{ $article->name }}</div>
+                    <div class="foto"><img src="{{ $article->photo }}" alt="Cukierki" class="responsive"></div>
+                    {{--  <div class="opisTowaru">{{ $article->description }}</div>  --}}
+                    <div class="cena">Cena: {{ number_format($article->price / 100, 2, ',', ' ') }} zł</div>
+                    {{--  <div class="dodaj"><a href="#">Dodaj do koszyka</a>&nbsp;
+                        <i class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>
+                    </div>  --}}
+                </div>
 
             @endforeach
+    </div>
     @else
         <h3>Brak artykułów</h3>
     @endif
