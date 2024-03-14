2bc2c1703d26952f2a84d74f6b871d4f8057d837
waldiw <wwilke@wp.pl>
2024-03-14 11:42:07 +0100

    podsumowanie koszyka

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -76,5 +76,109 @@
 @endsection
 
 @section('script')
-    @include('scripts.cartShow')
+     <script>
+        $(document).ready(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+    
+            cartload();
+    
+            $('.articleQuantity').change(function (e) {
+                e.preventDefault();
+    
+                var productId = $(this).closest('tr').find('.productId').val();
+                var quantity = $(this).closest('tr').find('.articleQuantity').val();
+    
+                $.ajax({
+                    url: '/update-cart',
+                    method: "POST",
+                    data: {
+                        'quantity': quantity,
+                        'product_id': productId,
+                    },
+                    success: function (response) {
+                        var value = jQuery.parseJSON(response); //Single Data Viewing
+                        $('#' + value['id']).html(numberFormat(value['subtotal']) + ' zł');
+                        cartload();
+                    },
+                });
+            });
+    
+            $(document).on('click', '.deleteCartData', function (e) {
+                e.preventDefault();
+    
+                var productId = $(this).closest('tr').find('.productId').val();
+    
+                $.ajax({
+                    url: '/delete-cart',
+                    method: "post",
+                    data: {
+                        'product_id': productId,
+                    },
+                    success: function () {
+                        $('#tr' + productId).remove();
+                        cartload();
+                    }
+                });
+    
+            });
+    
+            var modal = document.getElementById("confirmModal");
+            var span = document.getElementsByClassName("close")[0];
+            var btnNo = document.getElementsByClassName("btnNo")[0];
+    
+            $(document).on('click', '.btnDeleteCart', function (e) {
+                e.preventDefault();
+    
+                modal.style.display = "block";
+            });
+    
+            $(document).on('click', '.btnYes', function (e) {
+                e.preventDefault();
+    
+                $.ajax({
+                    url: '/clear-cart',
+                    method: "post",
+                    data: {},
+                    success: function () {
+                        $('#tableBody').remove();
+                        $('#order').remove();
+                        cartload();
+                        modal.style.display = "none";
+                    }
+                });
+            });
+    
+            // When the user clicks anywhere outside of the modal or span, close it
+            window.onclick = function (event) {
+                if (event.target === modal || event.target === span || event.target === btnNo) {
+                    modal.style.display = "none";
+                }
+            }
+    
+            function cartload() {
+                $.ajax({
+                    url: '/load-cart-data',
+                    method: "GET"
+                }).done(function (response) {
+                    var value = jQuery.parseJSON(response); //Single Data Viewing
+                    var tfoot = document.getElementsByTagName("tfoot");
+                    $('.basketItemCount').html(numberFormat(value['totalCart']) + ' zł');
+                    $('.basketTotal').html(numberFormat(value['totalCart']) + ' zł');
+                    if (value['totalCart'] === 0) {
+                        $(tfoot).remove();
+                        $('#order').remove();
+                    }
+                });
+            }
+    
+            function numberFormat($number) {
+                return ($number / 100).toLocaleString('pl-PL', {minimumFractionDigits: 2})
+            }
+    
+        });
+    </script>
 @endsection
